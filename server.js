869fad6b1c159bc9a2d6c6a3fb4840175718a18c
rishabh9785 <rishabhmendiratta9785@gmail.com>
2017-07-16 19:02:05 +0530

    edited server and sql

--- server.js
+++ server.js
@@ -8,51 +8,60 @@ var path=require('path');
 var port=4000||process.env.port;
 var app=express();
 var query1,query2;
+var dbquery=[];
+var tablequery=[];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/',express.static(path.join(__dirname,'Frontend')));
 app.get('/database/query1',function(req,res){
     query1=req.body.query1;
+    dbquery.push(query1);
+    console.log(query1);
+    console.log(dbquery[0]);
     res.send(query1);
 })
 app.get('/database/query2',function(req,res){
     query2=req.body.id;
+    tablequery.push(query2);
+    console.log(tablequery[0]);
+    console.log(query2);
     res.send(query2);
 })
 app.post('/insert/post',function(req,res){
-    var string1=query1;
-    var string2=query2;
-    if(string1==="cultural") {
+    var string1=dbquery[0];
+    console.log(string1);
+    var string2=tablequery[0];
+    if(string1==cultural) {
         if (string2 === frosh) {
-            var queryFinal = "Insert into frosh(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into frosh(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === saturnalia) {
-            var queryFinal = "Insert into saturnalia(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into saturnalia(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === spicmacay) {
-            var queryFinal = "Insert into spicmacay(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into spicmacay(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === tmc) {
-            var queryFinal = "Insert into tmc(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into tmc(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === cineyouth) {
-            var queryFinal = "Insert into cineyouth(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.conatct+"')";
+            var queryFinal = "INSERT into cineyouth(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.conatct+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
@@ -61,35 +70,35 @@ app.post('/insert/post',function(req,res){
     }
     else if(string1==="technical") {
         if (string2 === iete) {
-            var queryFinal = "Insert into iete(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into iete(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === ccs) {
-            var queryFinal = "Insert into ccs(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into ccs(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === iiche) {
-            var queryFinal = "Insert into iiche(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
+            var queryFinal = "INSERT into iiche(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact+"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === owasp) {
-            var queryFinal = "Insert into owasp(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact +"')";
+            var queryFinal = "INSERT into owasp(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact +"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
             })
         }
         else if (string2 === msc) {
-            var queryFinal = "Insert into msc(Name,Email,Contact) Values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact +"')";
+            var queryFinal = "INSERT into msc(Name,Email,Contact) values('" + req.body.name + "','" + req.body.email + "','" + req.body.contact +"')";
             sql.string1(queryFinal, function (data) {
                 console.log(data);
                 res.send(data);
--- sql.js
+++ sql.js
@@ -5,28 +5,36 @@ var mysql =require('mysql');
 
 var dbconfig1={
     host:'localhost',
-    username:'newuser',
+    user:'newuser',
     password:'newpassword',
     database:'cultural'
 }
 var dbconfig2={
     host:'localhost',
-    username:'newuser',
+    user:'newuser',
     password:'newpassword',
     database:'technical'
 }
 function cultural(query,cb,param){
     var connection=mysql.createConnection(dbconfig1);
+    console.log("db1 is connected");
     connection.connect();
     connection.query(query,function(err,data){
-         if(err) throw err
+         if(err) throw (err)
+         cb(data);
+         console.log(data);
+         connection.end();
     });
 }
 function technical(query,cb,param){
     var connection=mysql.createConnection(dbconfig2);
+    console.log("db2 is connected");
     connection.connect();
-    connection.query(query,function(data){
-
+    connection.query(query,function(err,data){
+        if(err) throw (err)
+        cb(data);
+        console.log(data);
+        connection.end();
     });
 }
 module.exports={
